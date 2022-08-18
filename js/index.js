acf83c3876728d01a17fd4e794fd53fdb780f9ac
mengnorov <ulugmurodmengnorov10@gmail.com>
2022-08-18 17:24:34 +0500

    create main

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded',function(){
     //bu ichiga yozilgan script codlar hamma html fayllarni ochib bo’gandan keyin ishlaydi    
     let close = document.getElementById('close-btn');
     let aside = document.getElementById('aside');
-        close.addEventListener('click', ()=>{
+    close.addEventListener('click', ()=>{
             aside.style.display = 'none';
         })
      })
