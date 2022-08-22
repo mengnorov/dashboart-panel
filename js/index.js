887384aa71f684790b5d5f210c6503a2d6a419ca
mengnorov <ulugmurodmengnorov10@gmail.com>
2022-08-23 01:29:03 +0500

    The end

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ window.addEventListener('DOMContentLoaded',function(){
     //bu ichiga yozilgan script codlar hamma html fayllarni ochib bo’gandan keyin ishlaydi    
     const sideMenu = document.querySelector('aside');
     const menuBtn = document.querySelector('#menu-btn');
-    const closeBtn = this.document.querySelector('#close-btn');
-    const themeToggler = this.document.querySelector('.theme-toggler')
+    const closeBtn = document.querySelector('#close-btn');
+    const themeToggler = document.querySelector('.theme-togler')
 
     //show sidebar
     menuBtn.addEventListener('click', () =>{
@@ -20,7 +20,9 @@ window.addEventListener('DOMContentLoaded',function(){
     //change theme
     themeToggler.addEventListener('click',() => {
         this.document.body.classList.toggle('dark-theme-variables');
-        document.body.style.backgroundColor = 'black'
+
+        themeToggler.querySelector('img:nth-child(1)').classList.toggle('active');
+        themeToggler.querySelector('img:nth-child(2)').classList.toggle('active');
     })
      })
     
